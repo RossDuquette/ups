bfb8e6850aecded87d14a4f55260b3e8a9ef5f24
Ross Duquette <rduquette@kepler.space>
2026-01-17 11:45:47 -0500

    Create directory structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RossDuquette/ups
+
+go 1.25.5
---
+++ target.mk
@@ -0,0 +1,25 @@
+GO_BUILD=go build -buildvcs=false
+BUILD_DIR=./bin
+
+clean:
+	@rm -rf $(BUILD_DIR)
+
+build: format build_broker build_pub build_sub
+
+format:
+	@go fmt ./...
+
+build_broker:
+	$(GO_BUILD) -o $(BUILD_DIR)/broker ./cmd/broker
+
+build_pub:
+	$(GO_BUILD) -o $(BUILD_DIR)/publisher ./cmd/publisher
+
+build_sub:
+	$(GO_BUILD) -o $(BUILD_DIR)/subscriber ./cmd/subscriber
+
+test: format
+	go test -v ./...
+
+lint:
+	@go vet
